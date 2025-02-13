fdc8ef7b73bf1f4a31ed2c26bd37ee69a678fa1e
Mariana Garcia <marianagarciamesa8@gmail.com>
2025-02-12 19:14:18 -0500

    JS

--- toDoTareas.js
+++ toDoTareas.js
@@ -4,15 +4,29 @@ function agregarTarea(){
     const nuevaTarea = document.getElementById("nuevaTarea");
     const textoTarea = nuevaTarea.value.trim();
 
+
+    //validar si es un vacío
+    if(textoTarea === ""){
+        alert("Agregue una tarea");
+        return;
+    }
+
     //capturamos el listado de tareas
     const listadoTarea = document.getElementById("listadoTareas");
 
 
-    const li = document.createElement("li");
-    li.innerHTML = `<span>${textoTarea}</span> <button class="borrar-btn">Eliminar</button>`;
+    const li = document.createElement("li"); //se crea un elemento <li></li>
+    li.innerHTML = `<span>${textoTarea}</span> 
+                        <button class="borrar-btn" onClick="borrarTarea(this)">Eliminar</button>`;
 
     listadoTarea.appendChild(li);
 
+    nuevaTarea.value = "";
 
+
+}
+
+function borrarTarea(elementoTarea){
+    elementoTarea.parentElement.remove();
 
 }
